8ce91b1a014a614fba1592b19b1d25ef9bba30a3
Rustam Suvarov <milord810@gmail.com>
2026-03-30 21:18:16 +0500

    lost files

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def get_mask_card_number(text: str) -> str:
+    """Функция, маскирующая номер карты"""
+    return text[0:4] + " " + text[4:6] + "**" + " " + "****" + " " + text[-4:]
+
+
+get_mask_card_number("7000792289606361")
+
+
+def get_mask_account(text: str) -> str:
+    """Функция, возвращающая ** и последние 4 цифры"""
+    return "**" + text[-4:]
+
+
+get_mask_account("73654108430135874305")
