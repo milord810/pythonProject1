56f0b98c500f2410f46fb794e3d720e32118ed26
Rustam Suvarov <milord810@gmail.com>
2026-04-04 12:21:03 +0500

    small fixes

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
 def get_mask_card_number(text: str) -> str:
     """Функция, маскирующая номер карты"""
-    return text[0:4] + " " + text[4:6] + "**" + " " + "****" + " " + text[-4:]
+    return str(text[0:4] + " " + text[4:6] + "**" + " " + "****" + " " + text[-4:])
 
 
 get_mask_card_number("7000792289606361")
@@ -8,7 +8,7 @@ get_mask_card_number("7000792289606361")
 
 def get_mask_account(text: str) -> str:
     """Функция, возвращающая ** и последние 4 цифры"""
-    return "**" + text[-4:]
+    return str("**" + text[-4:])
 
 
 get_mask_account("73654108430135874305")
--- src/widget.py
+++ src/widget.py
@@ -4,17 +4,17 @@ from masks import get_mask_account, get_mask_card_number
 def mask_account_card(number_of_count: str) -> str:
     """Функция, которая маскирует принятый номер карты/счета"""
     if "Visa Platinum " in number_of_count:
-        return "Visa Platinum " + get_mask_card_number(number_of_count[15:])
+        return str("Visa Platinum " + get_mask_card_number(number_of_count[15:]))
     elif "Maestro " in number_of_count:
-        return "Maestro " + get_mask_card_number(number_of_count[9:])
+        return str("Maestro " + get_mask_card_number(number_of_count[9:]))
     elif "MasterCard " in number_of_count:
-        return "MasterCard " + get_mask_card_number(number_of_count[12:])
+        return str("MasterCard " + get_mask_card_number(number_of_count[12:]))
     elif "Visa Classic " in number_of_count:
-        return "Visa Classic " + get_mask_card_number(number_of_count[13:])
+        return str("Visa Classic " + get_mask_card_number(number_of_count[13:]))
     elif "Счет " in number_of_count:
-        return "Счет " + get_mask_account(number_of_count[5:])
+        return str("Счет " + get_mask_account(number_of_count[5:]))
     else:
-        return "Неверный формат данных"
+        return str("Неверный формат данных")
 
 
 mask_account_card("MasterCard 7158300734726758")
@@ -22,7 +22,7 @@ mask_account_card("MasterCard 7158300734726758")
 
 def get_date(input_date: str) -> str:
     """Функция принимает на вход строку и выдает дату в требуемом формате"""
-    return input_date[8:10] + "." + input_date[5:7] + "." + input_date[0:4]
+    return str(input_date[8:10] + "." + input_date[5:7] + "." + input_date[0:4])
 
 
 get_date("2024-03-11T02:26:18.671407")
