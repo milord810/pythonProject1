b21d272b12565254a2f36958b808a54b3408add1
Rustam Suvarov <milord810@gmail.com>
2026-04-17 20:30:07 +0500

    fixed processing.py

--- src/processing.py
+++ src/processing.py
@@ -6,15 +6,13 @@ states = (
 )
 
 
-def filter_by_state(states: tuple) -> tuple[any]:
+def filter_by_state(states: list) -> list[list]:
     """Функция принимает список словарей и опционально значение для ключа
     state"""
-    filtered_states = [state for state in states if state["state"] == "EXECUTED"]
-    for state in filtered_states:
-        return filtered_states
+    return [state for state in states if state["state"] == "EXECUTED"]
 
 
-def sort_by_date(states: tuple) -> dict[any]:
+def sort_by_date(states: tuple) -> list[list]:
     """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки"""
     sorted_states = sorted(states, key=lambda states: states["date"], reverse=True)
     return sorted_states
