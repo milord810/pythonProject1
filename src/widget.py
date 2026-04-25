d34e5840718b9dce22ebe9ee9d56ecaf2efccfa0
Rustam Suvarov <milord810@gmail.com>
2026-04-25 20:27:32 +0500

    fixed tests

--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,22 @@
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(number_of_count: str) -> str:
+def mask_account_card(text: str) -> str:
     """Функция, которая маскирует принятый номер карты/счета"""
-    if "Visa Platinum " in number_of_count:
-        return str("Visa Platinum " + get_mask_card_number(number_of_count[15:]))
-    elif "Maestro " in number_of_count:
-        return str("Maestro " + get_mask_card_number(number_of_count[9:]))
-    elif "MasterCard " in number_of_count:
-        return str("MasterCard " + get_mask_card_number(number_of_count[12:]))
-    elif "Visa Classic " in number_of_count:
-        return str("Visa Classic " + get_mask_card_number(number_of_count[13:]))
-    elif "Счет " in number_of_count:
-        return str("Счет " + get_mask_account(number_of_count[5:]))
+    if not text:
+        return "Данные не введены"
+    splited_text = text.split()
+    card_or_account_info = " ".join(splited_text[:-1])
+    number = splited_text[-1]
+
+    if number.isdigit() and len(number) == 20:
+        return f"{card_or_account_info} {get_mask_account(number)}"
+    elif number.isdigit() and len(number) == 16:
+        return f"{card_or_account_info} {get_mask_card_number(number)}"
     else:
-        return str("Неверный формат данных")
+        return "Неверный формат данных"
 
 
 def get_date(input_date: str) -> str:
     """Функция принимает на вход строку и выдает дату в требуемом формате"""
     return str(input_date[8:10] + "." + input_date[5:7] + "." + input_date[0:4])
-
