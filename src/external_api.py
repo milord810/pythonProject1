c69cd3f00e3788991679f29ee9a97768418ef048
Rustam Suvarov <milord810@gmail.com>
2026-07-21 22:40:21 +0500

    fixes test_external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# API_KEY = os.getenv("API_KEY")
-API_KEY="PTYyVLoFiWALhiALPA5JaxkefFEz35cU"
+API_KEY = os.getenv("API_KEY")
 
 
 def convertation(data: dict) -> float:
@@ -16,30 +15,10 @@ def convertation(data: dict) -> float:
     headers = {"apikey": API_KEY}
 
     if currency == "RUB":  # Проверка, если валюта - RUB, возвращает значение
-        return amount
+        return float(amount)
 
     url = f"https://api.apilayer.com/currency_data/convert?to=RUB&from={currency}&amount={amount}"
 
-    response = requests.request("GET", url, headers=headers).json()
+    response:dict = requests.get(url, headers=headers).json()
 
-    return response
-
-
-if __name__ == "__main__":
-    transaction = {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-  }
-
-print(convertation(transaction))
+    return response.get("result")
