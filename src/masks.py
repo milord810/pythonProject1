51d9531ed5d13650b6f6b622225960670913a894
Rustam Suvarov <milord810@gmail.com>
2026-08-04 19:55:04 +0500

    added logging for utils.py and masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,24 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s: masks.py: %(levelname)s: %(message)s", filename="masks.log", filemode="w"
+)
+
+masks_logger = logging.getLogger("get_mask_card_number")
+file_handler = logging.FileHandler("masks.log")
+file_formatter = logging.Formatter("%(asctime)s: %(modulename)s: %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+masks_logger.addHandler(file_handler)
+masks_logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(text: str) -> str:
     """Функция, маскирующая номер карты"""
+    masks_logger.info("starting mask_card_number...")
     if len(text) != 16:
+        masks_logger.error("invalid value...")
         return "Недопустимое значение"
+    masks_logger.info("return mask card number...")
     return str(text[0:4] + " " + text[4:6] + "**" + " " + "****" + " " + text[-4:])
 
 
@@ -10,8 +27,11 @@ get_mask_card_number("7000792289606361")
 
 def get_mask_account(text: str) -> str:
     """Функция, возвращающая ** и последние 4 цифры"""
+    masks_logger.info("starting get mask account...")
     if len(text) != 20:
+        masks_logger.error("invalid value...")
         return "Недопустимое значение"
+    masks_logger.info("return mask account...")
     return str("**" + text[-4:])
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,29 @@
 import json
+import logging
 import os
+
 from dotenv import load_dotenv
+from mypy.cache import read_json
 
 load_dotenv(".env")
 
 API_KEY = os.getenv("API_KEY")
 
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s: utils.py: %(levelname)s: %(message)s", filename="utils.log", filemode="w"
+)
 
-def read_json(filename):
+logger = logging.getLogger("read_json")
+file_handler = logging.FileHandler("utils.log")
+file_formatter = logging.Formatter("%(asctime)s: %(modulename)s: %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+def reading_json(filename):
     try:
+        logger.info("starting app...")
         with open(filename, encoding="UTF-8") as file:
             data = json.load(file)
             if type(data) is list:
@@ -16,23 +31,18 @@ def read_json(filename):
             else:
                 return []
     except (FileNotFoundError, json.JSONDecodeError):
+        logger.error("failed starting app...")
         return []
 
 
 if __name__ == "__main__":
     print(read_json("../data/operations.json"))
     transaction = {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-  }
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560",
+    }
