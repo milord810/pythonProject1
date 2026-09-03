0ecfcfe88d663d785f8675dab26e8d51bc10694a
Rustam Suvarov <milord810@gmail.com>
2026-09-03 21:27:57 +0500

    small fixes data_frames.py

--- src/data_frames.py
+++ src/data_frames.py
@@ -6,16 +6,16 @@ import pandas as pd
 def csv_reader(way):
     """Функция для считывания CSV"""
     with open(way) as file:
-        reader = csv.DictReader(file, delimiter=",")
-        next(reader)
+        reader = csv.DictReader(file, delimiter=';')
         for row in reader:
-            return row
+            print(row['id'], row['state'], row['amount'], row['currency_name'], row['currency_code'])
 
 
 def excel_reader(excel_way):
     """Функция для считывания Excel"""
-    excel_data = pd.read_excel(excel_way)
-    print(excel_data.head())
+    excel_df = pd.read_excel(excel_way)
+    print(excel_df.shape)
+    print(excel_df.head())
 
 
 if __name__ == "__main__":
